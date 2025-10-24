14343f5f96e929efc3212e8beaea2721c0c64d71
logbasem <loganmichellemurphy@gmail.com>
2025-10-23 21:14:38 -0500

    Made the story more customized and fun

--- haunted_house.py
+++ haunted_house.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python3
 """
-A spooky Halloween-themed choose your own adventure game!
+A spooky Halloween-themed choose your own adventure game! (Brought to you by Ghouls WhoOooOoo CoOoooOOoode)
 Welcome to the haunted house...
 """
 
 def main():
     print("🎃 Welcome to the HAUNTED HOUSE! 🎃")
     print("=" * 50)
-    print("\nYou stand before an old, creaky mansion on Halloween night.")
-    print("The wind howls through the dead trees, and thunder rumbles overhead.")
-    print("The front door is slightly ajar, and you can hear strange noises from within...")
+    print("\nIt's Halloween night, and instead of trick-or-treating or drinking spookily too many potions at a party,")
+    print(" you've found yourself on the front doorstep of the old, abandoned mansion at the edge of OU campus. Thunder rumbles ")
+    print("in the clouds above, and you swear you hear a soft whispering in the wind around you, which cuts through your clothes with an icy chill.")
+    print(" As you gaze at the house before you, you notice the front door is slightly ajar...")
     print()
+    print("What do you do?")
+    print("1. Enter through the front door")
+    print("2. Try to get in through the window")
     
     # Initial choice variable
     initial_choice = ""
     
-    # First question - use while loop for input validation
+    # First question
     while initial_choice not in ["1", "2"]:
-        print("What do you do?")
-        print("1. Enter through the front door")
-        print("2. Sneak around to the back of the house")
         initial_choice = input("\nEnter your choice (1 or 2): ").strip()
         
         if initial_choice not in ["1", "2"]:
@@ -30,17 +30,19 @@ def main():
         # Path 1: Front door
         print("\n" + "=" * 50)
         print("You push open the heavy front door...")
-        print("It creaks loudly as you step into a dusty foyer.")
-        print("A grand staircase rises before you, and to your left is a dimly lit hallway.")
-        print("You hear scratching sounds coming from upstairs.")
+        print("It creeeaaaaks open under your touch. As you enter, your footsteps echo on the dusty wooden floor.")
+        print("To your left, you see a beautiful, dust-covered grand piano, with several keys missing.")
+        print("To your right, you see a spiraling wooden staircase.")
+        print("As you walk further, all of the sudden you hear what sounds like a mournful, drawn-out howl coming from above you.")
+        print("It could be the wind, but you have an awful feeling that the sound came from upstairs...")
         print()
+        print("What do you do next?")
+        print("1. Go upstairs to investigate the sound")
+        print("2. Play a jam on the piano")
         
         # Second choice for path 1
         path1_choice = ""
         while path1_choice not in ["1", "2"]:
-            print("What do you do next?")
-            print("1. Climb the creaky staircase")
-            print("2. Explore the dark hallway")
             path1_choice = input("\nEnter your choice (1 or 2): ").strip()
             
             if path1_choice not in ["1", "2"]:
@@ -50,59 +52,57 @@ def main():
         if path1_choice == "1":
             print("\n" + "=" * 50)
             print("You start climbing the staircase...")
-            print("Each step groans under your weight...")
+            print("Each step groans under your weight, but eventually, you reach the top. There, you find two doors.")
             print()
-            # TODO: New programmers - complete this path!
-            print("🔮 TO BE CONTINUED... 🔮")
-            print("(New programmers: Add your own spooky ending here!)")
+            print("Which door do you choose?")
+            print("1. The door on the left")
+            print("2. The door on the right")
+            door_choice = ""
+            while door_choice not in ["1", "2"]:
+                door_choice = input("\nEnter your choice (1 or 2): ").strip()
+                
+                if door_choice not in ["1", "2"]:
+                    print("\n❌ Invalid choice! Please enter 1 or 2.\n")
+
+            if door_choice == "1":
+                print("\n" + "=" * 50)
+                print("INSERT YOUR SPOOKY ENDING FOR LEFT DOOR HERE!")
+                print("CHOICE 1")
+                print("CHOICE 2")
+                left_door_choice = ""
+                # TODO: New programmers - complete this path!
+            else:
+                print("\n" + "=" * 50)
+                print("INSERT YOUR SPOOKY ENDING FOR RIGHT DOOR HERE!")
+                print("CHOICE 1")
+                print("CHOICE 2")
+                right_door_choice = ""
+                # TODO: New programmers - complete this path!
             
         else:  # path1_choice == "2"
             print("\n" + "=" * 50)
-            print("You cautiously walk down the hallway...")
-            print("Portraits on the walls seem to watch you...")
+            print("Ignoring the sound, you walk over to the grand piano, taking a seat at the creaky bench in front of it.")
+            print("Though the piano looks certainly past its prime, you decide to try playing a couple keys.")
+            print("To your surprise, the piano is perfectly in tune! Excited by this development, you play a little tune.")
+            print("The piano is your loyal companion, and you understand its every need and whim. Before you know it, the tune is really picking up!")
+            print("So enraptured by the song, you hardly even notice when another figure sits on the bench beside you, turning your solo-jam into a duet.")
+            print("The figure has rotting, green skin, and an affinity for brains.")
+            print("You also don't notice when a figure dressed all in black appears from a nearby coffin, taking out a violin.")
+            print("Before you know it, you've got a full quartet formed in the center of the house... and you sound pretty good!")
+            print("Even when you realize, you don't seem to mind. You spend the rest of the night playing with your newfound musical friends.")
             print()
-            # TODO: New programmers - complete this path!
-            print("🔮 TO BE CONTINUED... 🔮")
-            print("(New programmers: Add your own spooky ending here!)")
+            print("GAME OVER! You played the monster mash, and it was a graveyard smash. Try again to see what other haunts await you in the house!")
+
     
     else:  # initial_choice == "2"
         # Path 2: Back of house
         print("\n" + "=" * 50)
-        print("You carefully make your way around the side of the mansion...")
-        print("Thorny bushes scratch at your clothes as you creep along.")
-        print("You reach the backyard and see a cellar door and a greenhouse.")
-        print("Strange green light glows from inside the greenhouse.")
+        print("Not one to trust an open door, you make your way to the side of the house.")
+        print("Thorns claw at your legs as you trudge along. Eventually, you find a nice big rock to break the window with.")
+        print("You use the rock to shatter the glass, and carefully climb through the jagged, makeshift entrance.")
+        print("You fall on a spike trap and die!")
         print()
-        
-        # Second choice for path 2
-        path2_choice = ""
-        while path2_choice not in ["1", "2"]:
-            print("What do you do next?")
-            print("1. Open the cellar door")
-            print("2. Investigate the glowing greenhouse")
-            path2_choice = input("\nEnter your choice (1 or 2): ").strip()
-            
-            if path2_choice not in ["1", "2"]:
-                print("\n❌ Invalid choice! Please enter 1 or 2.\n")
-        
-        # Branch for second level choice
-        if path2_choice == "1":
-            print("\n" + "=" * 50)
-            print("You pull open the heavy cellar door...")
-            print("Cold air rushes up from the darkness below...")
-            print()
-            # TODO: New programmers - complete this path!
-            print("🔮 TO BE CONTINUED... 🔮")
-            print("(New programmers: Add your own spooky ending here!)")
-            
-        else:  # path2_choice == "2"
-            print("\n" + "=" * 50)
-            print("You approach the greenhouse...")
-            print("The green light pulses rhythmically...")
-            print()
-            # TODO: New programmers - complete this path!
-            print("🔮 TO BE CONTINUED... 🔮")
-            print("(New programmers: Add your own spooky ending here!)")
+        print("GAME OVER! YOU DIED! Maybe we'll see you again as a ghost... Try again to see if you can make it farther than a single step!")
     
     print("\n" + "=" * 50)
     print("Thanks for playing! Happy Halloween! 👻")
